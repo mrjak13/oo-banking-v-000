4d6f78c0df942427f6fc9497a4fcfc0fbed9d029
Justin Kenney <justinkenney13@gmail.com>
2018-06-11 22:45:35 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,9 @@
 class BankAccount
-  attr_accessor :name
+  attr_accessor :balance, :status
+  attr_reader :name
 
-  def initialize(name)
+  def initialize(name, balance=1000, status="open")
     @name = name
-    
   end
 
 end
