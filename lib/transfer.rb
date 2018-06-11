5edd1b36d513ac7e04e966257392528cefcb75f4
Justin Kenney <justinkenney13@gmail.com>
2018-06-11 23:37:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,13 +13,11 @@ class Transfer
   end
 
   def execute_transaction
-    if self.status != "complete"
-      sender.balance = sender.balance - amount
-      receiver.balance = receiver.balance + amount
-      self.status = "complete"
-    else self.status == "complete"
-      "Transfer rejected. Please check your account balance."
-    end
-    binding.pry
+    sender.balance = sender.balance - amount
+    receiver.balance = receiver.balance + amount
+    self.status = "complete"
+    self.status == "complete" ? "Transfer rejected. Please check your account balance." : nil
+    
+    
   end
 end
