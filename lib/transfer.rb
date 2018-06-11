41feb353affbc3363936ce46b3f2370e1919d49b
Justin Kenney <justinkenney13@gmail.com>
2018-06-11 23:09:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   # your code here
-  attr_accessor :sender, :receiver, :status, :amount
+  attr_accessor :sender, :receiver, :status, :amount, :bankaccount
   def initialize(sender, receiver, status="pending", amount)
     @sender = sender
     @receiver = receiver
@@ -9,6 +9,6 @@ class Transfer
   end
 
   def valid?
-
+    binding.pry
   end
 end
