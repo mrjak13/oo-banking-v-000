b8641f8431dd15b6f6d31b3ed61097c3340ff0ff
Justin Kenney <justinkenney13@gmail.com>
2018-06-12 05:40:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,8 @@ class Transfer
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       self.status = "complete"
+    else self.status == "complete" && sender.valid? == false
+      self.status = "rejected"
     end
   end
 
