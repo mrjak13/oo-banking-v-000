f13026a8a5fd25b9dcd1bad31f290d6a100d1d15
Justin Kenney <justinkenney13@gmail.com>
2018-06-12 06:12:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,9 +18,9 @@ class Transfer
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       self.status = "complete"
-    elsif sender.valid? == false 
+    elsif sender.valid? == false
       self.status = "rejected"
-      # binding.pry
+      binding.pry
     elsif self.status == "rejected"
       "Transaction rejected. Please check your account balance."
     end
