39bd4d77859d2596a2a0c248959dd1a2a2eab525
Justin Kenney <justinkenney13@gmail.com>
2018-06-12 05:12:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.status != "complete" && sender.valid? == true
+    if self.status == "pending" && sender.valid? == true
       # binding.pry
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
